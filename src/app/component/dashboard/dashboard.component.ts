1db8f1708e0c9434192bd44bbdde72bf9cc1b243
AnawaratneMANA <it19086408@my.sliit.lk>
2023-02-28 14:41:48 +0530

    COMMIT: Add component specific implementation

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/model/task';
+import { CrudService } from 'src/app/service/crud.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  taskObj : Task = new Task();
+  taskArr : Task[] = [];
+  addTaskValue: string = '';
+  constructor(private crudService: CrudService) {}
+  ngOnInit(): void {
+    this.addTaskValue = '';
+    this.taskObj = new Task();
+    this.taskArr = [];
+    this.getAllTasks();
+  }
+  getAllTasks() {
+    this.crudService.getAllTask().subscribe(res => {
+      this.taskArr = res;
+    }, err => {
+      alert("Unable to get the Tasks! : " + err)
+    })
+  }
+
+  addTask(){
+    this.taskObj.task_name = this.addTaskValue;
+    this.crudService.addTask(this.taskObj).subscribe(res => {
+      this.ngOnInit(); //This is to refresh the list.
+      this.addTaskValue = '';
+    }, err => {
+      alert("Unable to add the Tasks! : " + err);
+    })
+  }
+
+  editTask(){
+    this.crudService.editTask(this.taskObj).subscribe(res => {
+      this.ngOnInit();
+    }, err => {
+      alert("Failed to update the tasks!");
+    })
+  }
+
+  deleteTask(etask: Task){
+    this.crudService.deleteTask(etask).subscribe(res => {
+      this.ngOnInit();
+    }, err => {
+      alert("Failed to delete task! : " + err);
+    })
+  }
 
 }
